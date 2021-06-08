65f1e3bd06ca750a89520d7d468f28a485805a61
juyoung <jyleegoo@gmail.com>
2021-06-09 00:52:05 +0900

    Second commit

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -10,6 +10,6 @@ public class Controller {
 	@GetMapping("/")
 	public String get() {
 		System.out.println("get...");
-		return "{\"id\": \"1234567890\"}";			
+		return "{\"id\": \"1234567890, edit for HW\"}";			
 	}
 }
